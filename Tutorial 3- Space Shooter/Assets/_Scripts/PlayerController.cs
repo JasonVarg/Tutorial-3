8abfb2fd383dc72b31e953c7ae8c55770f858139
Jason Vargas <javarg410@knights.ucf .edu>
2019-07-22 22:30:30 -0400

    Scoring implemented

--- Tutorial 3- Space Shooter/Assets/_Scripts/PlayerController.cs
+++ Tutorial 3- Space Shooter/Assets/_Scripts/PlayerController.cs
@@ -19,10 +19,12 @@ public class PlayerController : MonoBehaviour
 
      private float nextFire;
      private Rigidbody rb;
+     private AudioSource audioSource;
 
      private void Start()
      {
-          rb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+        rb = GetComponent<Rigidbody>();
      }
 
      void Update()
@@ -31,6 +33,8 @@ public class PlayerController : MonoBehaviour
          {
             nextFire = Time.time + fireRate;
             Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+
+            audioSource.Play();
          }
      }
      void FixedUpdate()
