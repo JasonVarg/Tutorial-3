45de3a88e5f17b7860d160624ccca31474d9fb9a
Jason Vargas <javarg410@knights.ucf .edu>
2019-07-26 14:00:05 -0400

    Created the framework for an enemy ship

--- Tutorial 3- Space Shooter/Assets/_Scripts/DestroyByContact.cs
+++ Tutorial 3- Space Shooter/Assets/_Scripts/DestroyByContact.cs
@@ -37,7 +37,7 @@ public class DestroyByContact : MonoBehaviour
             gameController.GameOver ();
         }
 
-        gameController.AddScore(scoreValue);
+//      gameController.AddScore(scoreValue);
         Destroy(other.gameObject);
         Destroy(gameObject);
     }
