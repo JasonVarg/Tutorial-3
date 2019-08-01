564da14b6e5721bdf9ec305cf4d5785a725fb3ff
Jason Vargas <javarg410@knights.ucf .edu>
2019-08-01 19:14:34 -0400

    PowerUp is not working, considering removing PowerUp

--- Tutorial 3- Space Shooter/Assets/_Scripts/GameController.cs
+++ Tutorial 3- Space Shooter/Assets/_Scripts/GameController.cs
@@ -10,7 +10,6 @@ public class GameController : MonoBehaviour
     public Vector3 spawnValues;
 
     public int hazardCount;
-
     public float spawnWait;
     public float startWait;
     public float waveWait;
@@ -95,17 +94,18 @@ public class GameController : MonoBehaviour
         
         while (true)
         {
+
             for (int i = 0; i < hazardCount; i++)
             {
                 GameObject hazard = hazards[Random.Range (0,hazards.Length)];
-
                 Vector3 spawnPosition = new Vector3 (Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
                 Quaternion spawnRotation = Quaternion.identity;
-
+                
                 Instantiate(hazard, spawnPosition, spawnRotation);
 
                 yield return new WaitForSeconds (spawnWait);
             }
+            
             yield return new WaitForSeconds (waveWait);
 
             if(gameOver)
